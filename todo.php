e0fbf0134cb6ddf7219a9e41602309746358d6dc
JLo <jkloehman@Jasons-MacBook-Air.local>
2014-05-19 13:36:15 -0500

    with array order function

--- todo.php
+++ todo.php
@@ -2,6 +2,7 @@
 
 $items = array();
 
+
 function list_items($list)
 {
     $result ='';
@@ -17,9 +18,34 @@ function get_input($upper = false)
     return $upper ? strtoupper($result) : $result;
 }
 
+function sort_menu($items)
+{
+           echo '(A)-Z, (Z)-A, (O)rder entered, (R)everse order : ';
+        // sort the $list
+        //put your code here
+        switch(get_input(true)) {
+     case 'A':
+        asort($items);
+        break;
+        // Output Monday
+     case 'Z':
+        arsort($items);
+        break;
+     case 'O':
+        ksort($items);
+        break;
+     case 'R':
+        krsort($items);
+        break;
+
+} return($items);
+ 
+}
+
+
 do {
     echo list_items($items);
-    echo '(N)ew item, (R)emove item, (Q)uit : ';
+    echo '(N)ew item, (R)emove item, (S)ort items, (Q)uit) : ';
 
     $input = get_input(true);
 
@@ -29,6 +55,7 @@ do {
 
         $items[] = get_input();
 
+
     } elseif ($input == 'R') {
         echo 'Enter item number to remove: ';
 
@@ -37,7 +64,12 @@ do {
 
         unset($items[$key]);
 
-    } $items = array_values($items);
+    } elseif ($input == 'S') {
+       $items = sort_menu($items);
+
+}
+
+       
 
 } while ($input != 'Q');
 
